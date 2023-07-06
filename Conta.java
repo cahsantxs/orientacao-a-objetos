e7b8c1ce146391f4e4af88c91afa0adac19168c5
Camila Santos <cah.santxs@gmail.com>
2023-07-06 16:33:31 -0300

    Primeira Classe

---
+++ Conta.java
@@ -0,0 +1,18 @@
+package bytebank;
+
+//A palavra reservada 'class' em java define um tipo.
+class Conta { //As chaves ({}) indicam os atributos que estão contidos dentro da classe.
+
+	/*Em java, é necessário declarar o tipo da variável. E uma vez declarada, ela sempre 
+	 *corresponderá à esse tipo. */
+
+	double conta; //double: guarda ponto flutuante 
+	int numero; //int: guarda números inteiros
+	int agencia; // --------------------------------------
+	String titular; //String: guarda cadeias de caracteres
+	
+	/* Para se iniciar a execução de um programa no java, ele precisa de um
+	 * ponto de entrada: o método 'main'. Como nosso código não contém tal método,
+	 * ele não está executável. */
+	
+}
