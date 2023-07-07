f4c5b917060441f3bc9b1f8f0be3c9163e95aaa0
Camila Santos <cah.santxs@gmail.com>
2023-07-07 10:03:43 -0300

    Instanciação, atributos e referências

---
+++ CriaConta.java
@@ -0,0 +1,14 @@
+package bytebank;
+
+public class CriaConta {
+	public static void main(String[] args) {
+		new Conta(); /*A palavra reservada 'new' é utilizada no Java 
+					para criar uma nova instância (objeto) de uma classe.
+					Ela é usada para alocar na memória, espaço para suas características. 
+					Ao lado de 'new', utiliza-se o nome da Classe que se deseja criar um objeto.
+					- A partir da classe 'Conta', é criado um  objeto que contém: 
+					- conta, numero, agencia e titular.
+					Este é um código simples, mostrando como se instanciar um objeto.*/
+
+	}
+}
