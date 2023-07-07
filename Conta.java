a6a91b4a60b562cddc9a347cc7e7b871fd1a5ee4
Camila Santos <cah.santxs@gmail.com>
2023-07-07 10:43:17 -0300

    Primeira Classe - Contas

--- Conta.java
+++ Conta.java
@@ -1,19 +1,26 @@
 package bytebank;
 
-//A palavra reservada 'class' em java define um tipo.
-class Conta { //As chaves ({}) indicam os atributos que estão contidos dentro da classe.
+/* A palavra reservada 'class' em java é usada para declarar a definição de uma classe.
+ * A classe define a estrutura e o comportamento dos objetos que serão criados a partir dela.*/
 
-	/*Em java, é necessário declarar o tipo da variável. E uma vez declarada, ela sempre 
-	 *corresponderá à esse tipo. */
+class Conta { 	
 
-	double conta; //double: guarda ponto flutuante 
-	int numero; //int: guarda números inteiros
-	int agencia; // --------------------------------------
-	String titular; //String: guarda cadeias de caracteres
+			/* A função das chaves é definir o início e o fim de um bloco de código. 
+			 * Todas as instruções dentro de um bloco são consideradas parte desse bloco 
+			 * e são executadas em conjunto. */
+
+	/* Em Java é necessário declarar o tipo da variável ao utilizá-la.
+	 * A declaração do tipo de variável é uma prática fundamental 
+	 * em linguagens estáticas como Java, pois permite que o compilador 
+	 * verifique a corretude do código em tempo de compilação. */
+
+	double conta; //O tipo 'double' é usado para representar valores numéricos de ponto flutuante 
+	int numero; //O tipo 'int' é usado para representar valores inteiros
+	int agencia; // -----------------------------------------------------------------------------
+	String titular; //O tipo String é usado para representar sequências de caracteres (texto).
 	
-	/* Para se iniciar a execução de um programa no java, ele precisa de um
-	 * ponto de entrada: o método 'main'. Como nosso código não contém tal método,
-	 * ele não está executável. */
-	//
+	/* Para executar um programa em Java, é necessário ter um ponto de entrada, que é o método 'main'.
+	 * Esse método é o ponto de partida da execução e é onde o programa começa a ser executado.
+	 * Como esse código não contem o método main, ele não será executável.*/
 	
 }
